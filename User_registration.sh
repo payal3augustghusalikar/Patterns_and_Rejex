0afaf6c1e8bedf0e3618d72ceb9deed4bf01dd3f
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-14 14:34:03 +0530

    Added : code for uc-2 to enter Last Name

--- User_registration.sh
+++ User_registration.sh
@@ -12,3 +12,15 @@ then
 else
         echo "first Name is invalid"
 fi
+
+echo "Enter your Last Name and first letter should be Capital"
+read Last_Name
+
+   	pattern1="^([A-Z]+)[A-Za-z]{2,}]$"
+
+if [[ $Last_Name =~ $pattern1 ]]
+then
+   echo "Last Name is valid"
+else
+   echo "Last Name is invalid"
+fi
