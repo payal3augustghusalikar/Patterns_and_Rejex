56d80931dc5b74740e37a10230d0348dac527dd8
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-16 14:55:07 +0530

    Added : function for patterns

--- User_registration.sh
+++ User_registration.sh
@@ -1,62 +1,98 @@
 #! /bin/bash -x
-echo "Welcome to User Registration"
+echo "****** Welcome to User Registration ******"
 
-echo "Enter your first Name and first letter should be Capital"
-read Name
+#to get first name
 
-        pattern="^([A-Z]+)[a-zA-Z]{2,}$"
+function first_name()
 
-if [[ $Name =~ $pattern ]]
-then
-        echo "first Name is valid"
-else
-        echo "first Name is invalid"
-fi
+{
+	echo "Enter your first Name and first letter should be Capital"
+	read Name
 
-echo "Enter your Last Name and first letter should be Capital"
-read Last_Name
+   pattern="^([A-Z]{1})[a-zA-Z]{2,}$"
 
-   	pattern1="^([A-Z]+)[A-Za-z]{2,}]$"
+		if [[ $Name =~ $pattern ]]
+		then
+			echo "first Name is valid"
+		else
+			echo "first Name is invalid"
+		fi
+}
 
-if [[ $Last_Name =~ $pattern1 ]]
-then
-   echo "Last Name is valid"
-else
-   echo "Last Name is invalid"
-fi
+#to get last name
 
-echo "Enter your E-mail ID :"
-read Email
+function last_name()
+{
+	echo "Enter your Last Name and first letter should be Capital"
+	read Last_Name
+
+   pattern1="^([A-Z]{1})([A-Za-z]{2,})$"
+
+	if [[ $Last_Name =~ $pattern1 ]]
+	then
+   	echo "Last Name is valid"
+	else
+   	echo "Last Name is invalid"
+	fi
+}
+
+#to get email-id
+
+function email_ID()
+{
+	echo "Enter your E-mail ID :"
+	read Email
 
    pattern2="^([a-z]{3,})([\.a-z]*)@([a-z]{2})\.([a-z]{2})([\.]{1}[a-z]{2}){0,1}$"
 
-if [[ $Email =~ $pattern2 ]]
-then
-   echo "email is valid"
-else
-   echo "email is invalid"
-fi
+	if [[ $Email =~ $pattern2 ]]
+	then
+   	echo "email is valid"
+	else
+   	echo "email is invalid"
+	fi
+}
 
-echo "Enter your Mobile No. :"
-read Mob_No
+#to get mobile no.
+
+function mobile_No()
+{
+	echo "Enter your Mobile No. :"
+	read Mob_No
 
    pattern3="^([0-9]{2})\ ([1-9]{1}[0-9]{9})$"
 
-if [[ $Mob_No =~ $pattern3 ]]
-then
-   echo "Mobile no.  is valid"
-else
-   echo "Mobile no. is invalid"
-fi
+	if [[ $Mob_No =~ $pattern3 ]]
+	then
+   	echo "Mobile no.  is valid"
+	else
+   	echo "Mobile no. is invalid"
+	fi
+}
 
-echo "Enter your password :"
-read Password
 
-   pattern4="^([A-Z]?[0-9]?[\*\@\$\-\_]?[A-Za-z0-9]{5})$"
+#to get password
 
-if [[ $Password =~ $pattern4 ]]
-then
-   echo "Password is valid"
-else
-   echo "Password is invalid"
-fi
+function password()
+{
+	echo "Enter your password :"
+	read Password
+
+   pattern4="^([A-Z]+)([0-9]+)[@#$%^&*]{1}[0-9a-zA-Z]{5,22}$"
+
+	if [[ $Password =~ $pattern4 ]]
+	then
+   	echo "Password is valid"
+	else
+   	echo "Password is invalid"
+	fi
+}
+
+#function call
+
+echo "Please enter details as follws : "
+	first_name
+	last_name
+	email_ID
+	mobile_No
+	password
